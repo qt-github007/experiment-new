5b7b161c445db9abfeaed739971ec4387983d2fb
lixu <lixu@sh-stt.com>
2020-12-03 13:22:31 +0800

    测试

--- src/main/java/com/lxqtt/experiment/ExperimentApplication.java
+++ src/main/java/com/lxqtt/experiment/ExperimentApplication.java
@@ -8,6 +8,7 @@ public class ExperimentApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ExperimentApplication.class, args);
+        System.out.println("启动成功");
     }
 
 }
